62593fe7ef36a1e3a43cd5ce8d345480e3eb0920
achigua <andreshamt765@gmail.com>
2025-02-26 21:27:11 -0500

    clase 1 - poo

---
+++ src/poo_2025/clase_1.py
@@ -0,0 +1,11 @@
+
+class Personas():
+    def __init__(self):
+        #atributos del objeto
+        self.nombre = "Andres"
+        self.edad = 38
+        self.estatura = 170
+        self.peso = 71
+
+persona = Personas()        
+print("Nombre: ",persona.nombre,"- Edad: ",persona.edad,"años - Estatura: ",persona.estatura,"cms - Peso: ",persona.peso,"Kilogramos")
