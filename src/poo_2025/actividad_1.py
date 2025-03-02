67352e2a7766a8bba4f316857efb16562dfc40eb
bibiana_roman <bibiana.roman@est.iudigital.edu.co>
2025-03-02 17:58:10 +0000

    Actualizacion actividad_1 y setup

--- src/poo_2025/actividad_1.py
+++ src/poo_2025/actividad_1.py
@@ -10,5 +10,26 @@ class Ingestiones():
     def __init__(self):
         self.ruta_static = r"C:src\poo_2025\static"
 
-def leer_api(self, url):
-    response        
+    def leer_api(self, url):
+        # Realiza una solicitud GET a la API
+        response = requests.get(url)    
+        
+        # Verifica si la solicitud fue exitosa (código de estado 200)
+        if response.status_code == 200:
+            # Devuelve los datos en formato JSON
+            return response.json()
+        else:
+            # Si hubo un error, devuelve None o maneja el error según sea necesario
+            return None
+
+    def escribir_json(self, nombre, datos):
+        #r: read, w: write
+        ruta_json = "{}.json".format(nombre) 
+        with open(file = ruta_json, mode = "w", encoding = "utf-8") as f:
+            datos = json.dump(datos, f)
+        return datos
+
+ingestion = Ingestiones()
+#datos_json = ingestion.leer_api("https://api.github.com/users/octocat")
+datos_json = ingestion.leer_api("https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format=json&jscmd=data")
+print("Datos json: ", datos_json)            
