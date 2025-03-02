706c1381069caa7f13eb2e83d7a7dcea8e712a24
bibiana_roman <bibiana.roman@est.iudigital.edu.co>
2025-03-02 17:28:16 +0000

    carga inicial

--- src/poo_2025/actividad_1.py
+++ src/poo_2025/actividad_1.py
@@ -1,4 +1,14 @@
 
-from clase_1 import Personas
+#from clase_1 import Personas
 
-persona = Personas()
+#persona = Personas()
+
+import json
+import requests
+
+class Ingestiones():
+    def __init__(self):
+        self.ruta_static = r"C:src\poo_2025\static"
+
+def leer_api(self, url):
+    response        
