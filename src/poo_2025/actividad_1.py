ab823762d8838e81a987e9b4ed0f0cca11521b77
bibiana_roman <bibiana.roman@est.iudigital.edu.co>
2025-03-02 21:15:11 +0000

    escribir archivos txt

--- src/poo_2025/actividad_1.py
+++ src/poo_2025/actividad_1.py
@@ -9,12 +9,28 @@ import sys
 
 class Actividad_1():
     def __init__(self):
-        self.ruta_static = r"C:src\poo_2025\static"
+        self.ruta_static = r"C:\src\poo_2025\static"
         sys.stdout.reconfigure(encoding = 'utf-8')
+
+#*********************************************************************************************************
+
+    def escribir_txt(self, nombre_archivo = "", datos = None):
+        if nombre_archivo == "":
+            nombre_archivo = "datos.txt"
+        if datos is None:
+            datos = "No hay datos"
+        ruta_txt = "{}\txt\{}".format(self.ruta_static, nombre_archivo)
+        with open(ruta_txt, "w", encoding = 'utf-8') as f:
+            json.dump(datos, f, ensure_ascii = False, indent = 4)
+            f.write(str(datos))
+        return True        
+
+#*********************************************************************************************************
 
     def leer_api(self, url):
         # Realiza una solicitud GET a la API
-        response = requests.get(url)    
+        response = requests.get(url)
+        return response.json()    
         
         # Verifica si la solicitud fue exitosa (código de estado 200)
         if response.status_code == 200:
@@ -23,6 +39,8 @@ class Actividad_1():
         else:
             # Si hubo un error, devuelve None o maneja el error según sea necesario
             return None
+
+#*********************************************************************************************************
 
     def escribir_json(self, nombre, datos):
         #r: read, w: write
@@ -34,4 +52,7 @@ class Actividad_1():
 ingestion = Actividad_1()
 #datos_json = ingestion.leer_api("https://api.github.com/users/octocat")
 datos_json = ingestion.leer_api("https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format=json&jscmd=data")
-print("Datos json: ", datos_json)            
+print("Datos json: ", datos_json)
+if ingestion.escribir_txt(nombre_archivo = "datos.txt", datos = datos_json):
+    print("Se creo el archivo txt")
+#print("Esta es la ruta estatica: ", ingestion.ruta_static)          
