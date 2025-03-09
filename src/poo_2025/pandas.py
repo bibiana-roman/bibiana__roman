060d33fd8dae301646cb781fe9b6e43530d6ffa8
bibiana_roman <bibiana.roman@est.iudigital.edu.co>
2025-03-09 20:02:32 +0000

    cracion pandas

---
+++ src/poo_2025/pandas.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+class Matrices():
+    def __init__(self):
+        pass
+
+    def serie(self):
+        etiquetas = ["a","b","c"]
+        print (etiquetas)
+
+matr = Matrices()
+matr.serie()        
