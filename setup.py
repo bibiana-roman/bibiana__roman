504bc3698a85b91e8e6e61a7e4cca3b1f775a15f
bibiana_roman <bibiana.roman@est.iudigital.edu.co>
2025-03-22 11:44:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ setup(
     description = "",
     py_modules = ["actividad_1"],
     install_requires = [
+        "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
         "pandas",
-        "requests",
-        "matplotlib",
+        "numpy",
         "openpyxl",
-        "numpy"
+        "requests"
     ]
 )
